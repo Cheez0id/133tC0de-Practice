7b8e775d1a89c6177c72ced4d4fef179b6829289
Mary Prince <mleeherr@gmail.com>
2022-07-15 14:30:21 -0400

    learning about linked lists and list nodes.

--- solutions/leetcode2.js
+++ solutions/leetcode2.js
@@ -1,4 +1,5 @@
 //Leetcode 2. Add Two Numbers - Javascript (https://leetcode.com/problems/add-two-numbers/)
+//note for reference: https://www.freecodecamp.org/news/implementing-a-linked-list-in-javascript/
 
 
 // /**
@@ -13,6 +14,62 @@
 //  * @param {ListNode} l2
 //  * @return {ListNode}
 //  */
+
+// fuction to implement a list node. What this means is that we are defining how the NODE in the list behaves.  if the node has data it equals that data and the next one will be null.  I think what it means is that if there is something defined as .next the response will return null
+class ListNode{
+        constructor (data){
+            this.data = data
+            this.next = null
+        }
+}
+
+// implementing a linked list; remember, a constructor is a special function in javascript that Creates and Initalizes an object instance of a class - we are defining the functionality to create and set values for this new class. In this LinkedList class, the constructs is saying if the head node is not passed, the head is intalized as null.  SO I think that when we create a new list we have to define the head?
+class LinkedList{
+    constructor(head = null){
+        this.head = head
+    }
+}
+
+//time to create new list nodes (remember, these are just nodes at this point - they aren't in a linkedlist yet). I"m going to go ahead and make three of them to reflect the first of the two lists in the l33tcode challenge
+
+let node1 = new ListNode(2)
+let node2 = new ListNode(4)
+let node3 = new ListNode(3)
+// let node4 = new ListNode(7)
+
+//defining next node
+node1.next= node2
+node2.next = node3
+// node3.next = node4
+
+//create the linked list
+let list = new LinkedList(node1)
+
+// console log the nodes in the list we just made
+console.log(`${list.head.data} is the first node, ${list.head.next.data} is the second`)
+
+// this shows 2 and 4, respectively... let's see if we can look at the entire list and if the third number (3) is there
+console.log(list);
+// RETURNED:LinkedList {
+//   head: ListNode { data: 2, next: ListNode { data: 4, next: [ListNode] } }
+// }
+//out of curiosity, let's try 4 - i am curious if it will display the data for the third node; nope -t returned the same response for 'list'; probably because we made a new linked list with node 1, NOT with node 2 - hmm ANYWAY that was a sidetrack from the reference.  AHA here is the next piece to the puzzle
+
+// size() returns the number of nodes present in the linked list
+// clear() empties out the list
+//getLast() returnes the last node - or tail- of the linkes list
+//getFirst() returnes the head
+
+
+
+
+ var addTwoNumbers = function(l1, l2) {
+    
+};
+
+
+
+//----------------------COMMENTS BELOW CAN BE IGNORED -----------
 
 //OK! 
 // You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. OOOOH IT is L 1 and L 2, not 11 and 12!!!  question: how are the lists linked? because they are in the same line?
@@ -33,10 +90,3 @@
 // console.log(l1, l2);
 
 // linkedList(); 
-
-
-console.log(`hey this is a thing ${l1.head}`)
-
- var addTwoNumbers = function(l1, l2) {
-    
-};
