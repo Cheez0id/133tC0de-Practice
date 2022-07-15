1c87bef55500862995115a17718d456e0e182faa
Mary Prince <mleeherr@gmail.com>
2022-07-15 11:03:22 -0400

    modularization - hmm, I CAN export an array, but I NEED to export an object

--- index.js
+++ index.js
@@ -1,54 +1,11 @@
 
-//Leetcode two sum javascript
-let nums = [3,2,4];
-let target = 6;
+let solution1= require('./solutions/leetcode1')
 
-let twoSum = function (nums, target) {
-  for (i = 0; i < nums.length; i++) {
-    for (x = 0; x < nums.length; x++) {
-      if ((nums[i] + nums[x] == target) && (i !== x)) {
-          console.log([i, x]);
-		  return [i, x]
+// twoSum(nums, target);
+// console.log(solution1[0]);
+console.log(solution1);
 
-      } 
-    } 
-  } 
-};
+console.log(solution1.twoSum);
 
-twoSum(nums, target);
-
-// Runtime: 408 ms, faster than 5.01% of JavaScript online submissions for Two Sum.
-// Memory Usage: 43.1 MB, less than 29.84% of JavaScript online submissions for Two Sum.
-
-
-// // Write a function that takes in an integer and outputs the sum of all the numbers from 1 to that integer.
-
-// // Ex:
-// // Input: 2
-// // Output: 3 (because 1 + 2 = 3)
-
-// // Input: 4
-// // Output: 10  (because 1 + 2 + 3 + 4 = 10)
-
-// // Input: 10
-// // Output: 55
-
-// // Create a function as per instructions
-// // Added a SINGLE PARAMETER given that it said "takes in an integer"
-// function sumOfAllNumbers(int) {
-// 	// add from 1 -> that integer and display it.
-// 	// Created a variable to store the total integers added
-// 	var totalSum = 0;
-// 	// Created a for loop that will begin at the value of 1 and then end at the parameter input
-// 	for (var i = 1; i <= int; i++) {
-// 		// console.log(i);
-// 		// totalSum = totalSum + i;
-// 		// Reassigned to that existing variable of totalSum + the i.
-// 		totalSum += i;
-// 		// console.log(totalSum);
-// 	}
-// 	// Console.logs the totalSum value at the END of the loop
-// 	// LOOK UP RETURN STATEMENT AND WHAT IT DOES IF YOU WANT TO GET AHEAD :^)
-// 	console.log(totalSum);
-// };
-// sumOfAllNumbers();
+solution1.twoSum(solution1.nums, solution1.target);
+// console.log(solution1[0](solution[1], solution[2]))
--- solutions/leetcode1.js
+++ solutions/leetcode1.js
@@ -1,5 +1,6 @@
 
 //Leetcode 1. Two Sum - Javascript (https://leetcode.com/problems/two-sum/)
+
 let nums = [3,2,4];
 let target = 6;
 
@@ -15,7 +16,13 @@ let twoSum = function (nums, target) {
   } 
 };
 
-twoSum(nums, target);
+// let example = "meow";
+// let example2 = "meowwolf";
+
+module.exports = {twoSum, nums, target};
+// module.exports = [example, example2]
+
 
 // Runtime: 408 ms, faster than 5.01% of JavaScript online submissions for Two Sum.
 // Memory Usage: 43.1 MB, less than 29.84% of JavaScript online submissions for Two Sum.
+
